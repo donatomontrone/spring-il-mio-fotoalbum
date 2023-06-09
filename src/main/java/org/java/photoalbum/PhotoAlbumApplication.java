13d967342a9bf7cb7291a1b9f6f8fece74ce41a8
donatomontrone <donato.montrone@outlook.com>
2023-06-09 09:57:43 +0200

    setup repo

---
+++ src/main/java/org/java/photoalbum/PhotoAlbumApplication.java
@@ -0,0 +1,13 @@
+package org.java.photoalbum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAlbumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAlbumApplication.class, args);
+	}
+
+}
